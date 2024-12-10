c4bbc90a33a34f3b1f4b37864b7d75390439538b
matthew <matthewallanmacpherson@gmail.com>
2024-12-10 10:06:05 -0400

    Updated git ignore file.

---
+++ public/build/assets/ApplicationLogo-CADYOGsT.js
@@ -0,0 +1,1 @@
+import{j as o}from"./app-EiiXFY4f.js";function r(i){return o.jsx("img",{...i,src:"/images/fish.png"})}export{r as A};
---
+++ public/build/assets/PublicAppLayout-C4RE2de6.js
@@ -0,0 +1,1 @@
+import{j as a}from"./app-EiiXFY4f.js";function i({defaultViewName:u,children:t}){return a.jsx("div",{className:"PublicAppLayout",children:t})}export{i as default};
