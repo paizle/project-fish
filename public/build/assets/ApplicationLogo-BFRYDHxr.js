66a5d4d379982576b006aa101855862aec9753a5
matthew <matthewallanmacpherson@gmail.com>
2024-12-10 19:23:21 -0400

    Preparing deployment.

--- public/build/assets/ApplicationLogo-BFRYDHxr.js
+++
@@ -1,1 +0,0 @@
-import{j as o}from"./app-BFdlK44k.js";function r(i){return o.jsx("img",{...i,src:"/images/fish.png"})}export{r as A};
---
+++ public/build/assets/PublicAppNavigation-BpfbvZfQ.js
@@ -0,0 +1,1 @@
+import{j as t}from"./app-BrCgh0Fv.js";function a(){return t.jsx(t.Fragment,{})}export{a as default};
